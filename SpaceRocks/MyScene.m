8f82ef388c27bef4bb0e3fc784ea0bf088edb153
jpatrickburns <jpburns@gmail.com>
2014-02-13 12:12:06 -0500

    bouncing asteroids

--- SpaceRocks/MyScene.m
+++ SpaceRocks/MyScene.m
@@ -10,9 +10,38 @@
 
 @implementation MyScene
 
--(id)initWithSize:(CGSize)size {    
+
+static inline CGFloat skRandf() {
+    return rand() / (CGFloat) RAND_MAX;
+}
+
+static inline CGFloat skRand(CGFloat low, CGFloat high) {
+    return skRandf() * (high - low) + low;
+}
+
+
+- (void)addRock
+{
+    SKSpriteNode *rock = [SKSpriteNode spriteNodeWithImageNamed:@"rock"];
+    rock.size = CGSizeMake(64, 64);
+    rock.position = CGPointMake(skRand(0, self.size.width), self.size.height-50);
+    rock.name = @"rock";
+    rock.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:rock.size];
+    rock.physicsBody.restitution = .5;
+    rock.physicsBody.usesPreciseCollisionDetection = YES;
+    [self addChild:rock];
+}
+
+-(id)initWithSize:(CGSize)size {
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
+        
+        SKAction *makeRocks = [SKAction sequence: @[
+                                                    [SKAction performSelector:@selector(addRock) onTarget:self],
+                                                    [SKAction waitForDuration:0.50 withRange:0.15]
+                                                    ]];
+        [self runAction: [SKAction repeatActionForever:makeRocks]];
+        
         
         SKSpriteNode *myBG = [SKSpriteNode spriteNodeWithImageNamed:@"SpaceBG"];
         myBG.position=CGPointMake(self.size.width/2, self.size.height/2);
@@ -23,7 +52,20 @@
         //extract emitter
         SKEmitterNode *myRocks = [NSKeyedUnarchiver unarchiveObjectWithFile:myFile];
         myRocks.position = CGPointMake(self.size.width/2, self.size.height);
+        SKEmitterNode *rocksFlipped= [myRocks copy];
+        rocksFlipped.xScale=-1;
+        //add emitter
         [self addChild:myRocks];
+        [self addChild:rocksFlipped];
+        
+        //add saucer
+        SKSpriteNode *saucer = [SKSpriteNode spriteNodeWithImageNamed:@"saucer"];
+        saucer.size = saucer.texture.size;
+        saucer.position = CGPointMake(360, 500);
+        saucer.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(200, 90)];
+        saucer.physicsBody.dynamic = NO;
+        [self addChild:saucer];
+        
     }
     return self;
 }
