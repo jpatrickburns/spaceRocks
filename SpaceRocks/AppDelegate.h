6f66ce82cb6fb360175cac011a288b0bc210689f
jpatrickburns <jpburns@gmail.com>
2014-02-13 09:54:39 -0500

    Initial Commit

---
+++ SpaceRocks/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpaceRocks
+//
+//  Created by James Burns on 2/13/14.
+//  Copyright (c) 2014 James Burns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpaceRocks/ViewController.m
@@ -0,0 +1,51 @@
+//
+//  ViewController.m
+//  SpaceRocks
+//
+//  Created by James Burns on 2/13/14.
+//  Copyright (c) 2014 James Burns. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "MyScene.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Configure the view.
+    SKView * skView = (SKView *)self.view;
+    skView.showsFPS = YES;
+    skView.showsNodeCount = YES;
+    
+    // Create and configure the scene.
+    SKScene * scene = [MyScene sceneWithSize:skView.bounds.size];
+    scene.scaleMode = SKSceneScaleModeAspectFill;
+    
+    // Present the scene.
+    [skView presentScene:scene];
+}
+
+- (BOOL)shouldAutorotate
+{
+    return YES;
+}
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return UIInterfaceOrientationMaskAllButUpsideDown;
+    } else {
+        return UIInterfaceOrientationMaskAll;
+    }
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+@end
