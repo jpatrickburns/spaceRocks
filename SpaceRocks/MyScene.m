759b72ec2ef23b1110a5e0cff88fcb25435c041e
jpatrickburns <jpburns@gmail.com>
2014-02-13 11:01:25 -0500

    rocks falling over space

--- SpaceRocks/MyScene.m
+++ SpaceRocks/MyScene.m
@@ -14,16 +14,16 @@
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
+        SKSpriteNode *myBG = [SKSpriteNode spriteNodeWithImageNamed:@"SpaceBG"];
+        myBG.position=CGPointMake(self.size.width/2, self.size.height/2);
+        [self addChild:myBG];
         
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-        
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
-        
-        [self addChild:myLabel];
+        //make path to emitter file
+        NSString *myFile = [[NSBundle mainBundle] pathForResource:@"RockEmitter" ofType:@"sks"];
+        //extract emitter
+        SKEmitterNode *myRocks = [NSKeyedUnarchiver unarchiveObjectWithFile:myFile];
+        myRocks.position = CGPointMake(self.size.width/2, self.size.height);
+        [self addChild:myRocks];
     }
     return self;
 }
